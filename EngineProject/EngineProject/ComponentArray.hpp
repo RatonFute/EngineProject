b1e84700742fa61069a4deba44fa5e204bf87d80
Hugo Caroumbin | "RatonFuté <hcaroumbin@gaming.tech>
2023-10-25 17:25:29 +0200

    component and component manager

---
+++ EngineProject/EngineProject/ComponentArray.hpp
@@ -0,0 +1,59 @@
+#include "Type.hpp"
+#include <array>
+#include <cassert>
+#include <unordered_map>
+
+class IComponentArray
+{
+public:
+	virtual ~IComponentArray() = default;
+	virtual void EntityDestroyed(Entity entity) = 0;
+};
+
+
+template<typename T>
+class ComponentArray : public IComponentArray
+{
+public:
+	void insertComponent(Entity entity, T component)
+	{
+		assert(mEntityToIndexMap.find(entity) == mEntityToIndexMap.end() && "Component added to same entity more than once.");
+
+		size_t newIndex = mSize;
+		mEntityToIndexMap[newIndex] = entity;
+		mIndexToEntityMap[entity] = newIndex;
+		mComponentArray[newIndex] = component;
+		++mSize;
+	}
+	void removeData(Entity entity)
+	{
+		assert(mEntityToIndexMap.find(entity) != mEntityToIndexMap.end() && "data's entity does not exist")
+			size_t indexOfRemovedEntity = mEntityToIndexMap[entity];
+		size_t indexOfLastEntity = mSize - 1;
+
+		Entity entityOfLastElement = mIndexToEntityMap[indexOfLastEntity];
+		mEntityToIndexMap[entityOfLastElement] = indexOfLastEntity;
+		mIndexToEntityMap[indexOfLastEntity] = entityOfLastElement;
+
+		mEntityToIndexMap.erase(entity);
+		mIndexToEntityMap.erase(indexOfRemovedEntity);
+		--mSize;
+	}
+	T& GetData(Entity entity)
+	{
+		assert(mEntityToIndexMap.find(entity) != mEntityToIndexMap.end() && "data's entity does not exist");
+		return mComponentArray[mEntityToIndexMap[entity]];
+	}
+	void EntityDestroyed(Entity entity) override
+	{
+		if (mEntityToIndexMap.find(entity) != mEntityToIndexMap.end())
+		{
+			RemoveData(entity);
+		}
+	}
+private:
+	std::array<T, MAX_ENTITIES> mComponentArray{};
+	std::unordered_map<Entity, size_t> mEntityToIndexMap{};
+	std::unordered_map<size_t, Entity> mIndexToEntityMap{};
+	size_t mSize{};
+};
