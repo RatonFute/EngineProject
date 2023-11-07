9d9a456e6b1c5e5f7de5b266556eb309e914f025
LeannF <leann.farant@outlook.fr>
2023-11-07 09:20:21 +0100

    Rename main

--- EngineProject/EngineProject/Main.cpp
+++
@@ -1,9 +0,0 @@
-#include <d3d12.h>
-#include "Window.h"
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
-	PSTR cmdLine, int showCmd)
-{
-	Window::getInstance()->CreateGameWin(L"Window", 500, 500);
-	
-}
